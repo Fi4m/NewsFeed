7c564a944e51baa3e213942bdb892fe506b23ec4
Vedant Mahant <tscritch@yahoo.com>
2018-06-26 15:03:18 +0530

    Initial Commit

---
+++ NewsFeed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewsFeed
+//
+//  Created by Vedant Mahant on 26/06/18.
+//  Copyright © 2018 Vedant Mahant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
